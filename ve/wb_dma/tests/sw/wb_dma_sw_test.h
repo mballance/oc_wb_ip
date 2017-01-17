501fba7349b2095a6e03241ed198a017a6572acc
mballance-sf <matt.ballance@gmail.com>
2017-01-16 21:29:54 -0800

    Setup infrastructure for GoogleTest-driven DMA fw tests

---
+++ ve/wb_dma/tests/sw/wb_dma_sw_test.h
@@ -0,0 +1,11 @@
+
+#ifndef INCLUDED_WB_DMA_SW_TEST_H
+#define INCLUDED_WB_DMA_SW_TEST_H
+#include "wb_dma_uex_drv.h"
+#include "gtest/gtest.h"
+#include "uex.h"
+#include <stdio.h>
+
+extern wb_dma_uex_drv_t			wb_dma_drv;
+
+#endif /* INCLUDED_WB_DMA_SW_TEST_H */
