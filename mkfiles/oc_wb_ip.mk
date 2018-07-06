365a402ef4931aceb8fadd180a5e71570fc67b79
Matthew Ballance <matt.ballance@gmail.com>
2018-07-06 08:02:45 -0700

    Update ivpm build process

---
+++ mkfiles/oc_wb_ip.mk
@@ -0,0 +1,18 @@
+
+OC_WB_IP_MKFILES_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+OC_WB_IP_DIR := $(abspath $(OC_WB_IP_MKFILES_DIR)/..)
+
+
+ifneq (1,$(RULES))
+
+OC_WB_IP_JAR = $(OC_WB_IP_DIR)/lib/oc_wb_ip.jar
+
+OC_WB_IP_DEPS = $(STD_PROTOCOL_IF_JARS) $(CHISELLIB_JARS)
+
+OC_WB_IP_JARS = $(OC_WB_IP_JAR) $(OC_WB_IP_DEPS)
+
+else # Rules
+
+
+endif
+
--- scripts/ivpm.mk
+++ scripts/ivpm.mk
@@ -1,6 +1,65 @@
+#****************************************************************************
+#* ivpm.mk
+#*
+#* Makefile template for a Chisel project
+#****************************************************************************
+BUILD_DEPS = $(OC_WB_IP_JAR)
+SCRIPTS_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+ROOT_DIR := $(abspath $(SCRIPTS_DIR)/..)
+PROJECT := $(notdir $(ROOT_DIR))
+PACKAGES_DIR ?= $(ROOT_DIR)/packages
+LIB_DIR = $(ROOT_DIR)/lib
 
+ifneq (true,$(VERBOSE))
+Q=@
+endif
 
-build :
+# Must support dual modes: 
+# - build dependencies if this project is the active one
+# - rely on the upper-level makefile to resolve dependencies if we're not
+-include $(PACKAGES_DIR)/packages.mk
+include $(ROOT_DIR)/etc/ivpm.info
 
-clean :
+# Include makefiles with dependencies
+MK_INCLUDES += $(PACKAGES_DIR)/chiselscripts/mkfiles/chiselscripts.mk
+MK_INCLUDES += $(PACKAGES_DIR)/chisellib/mkfiles/chisellib.mk
+MK_INCLUDES += $(PACKAGES_DIR)/std_protocol_if/mkfiles/std_protocol_if.mk
+#MK_INCLUDES += $(PACKAGES_DIR)/sv_bfms/mkfiles/sv_bfms.mk
+MK_INCLUDES += $(wildcard $(ROOT_DIR)/mkfiles/*.mk)
+
+include $(MK_INCLUDES)
+
+SRC_DIRS += $(PROJECT)/wb_dma $(PROJECT)/wb_uart
+
+SRC := $(foreach dir,$(SRC_DIRS),$(wildcard $(ROOT_DIR)/src/$(dir)/*.scala))
+
+RULES := 1
+
+ifeq (true,$(PHASE2))
+build : $(BUILD_DEPS)
+	echo "BUILD_DEPS=$(BUILD_DEPS) OC_WB_IP_JAR=$(OC_WB_IP_JAR)"
+else
+build : $($(PROJECT)_deps)
+	$(Q)$(MAKE) -f $(SCRIPTS_DIR)/ivpm.mk PHASE2=true VERBOSE=$(VERBOSE) build
+endif
+
+# TODO: Change Makefile targets
+$(OC_WB_IP_JAR) : $(SRC) $(OC_WB_IP_DEPS)
+	$(Q)if test ! -d `dirname $@`; then mkdir -p `dirname $@`; fi
+	$(Q)$(DO_CHISELC)
+
+release : build
+	$(Q)rm -rf $(ROOT_DIR)/build
+	$(Q)mkdir -p $(ROOT_DIR)/build/$(PROJECT)
+	$(Q)cp -r \
+          $(ROOT_DIR)/lib \
+          $(ROOT_DIR)/etc \
+          $(ROOT_DIR)/build/$(PROJECT)
+	$(Q)cd $(ROOT_DIR)/build ; \
+		tar czf $(PROJECT)-$(version).tar.gz $(PROJECT)
+	$(Q)rm -rf $(ROOT_DIR)/build/$(PROJECT)
+
+include $(MK_INCLUDES)
+
+-include $(PACKAGES_DIR)/packages.mk
 
