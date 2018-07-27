e1211debde498a3afa10e2e9958b6bf5b1e5ef45
Matthew Ballance <matt.ballance@gmail.com>
2018-07-27 06:30:40 -0700

    cleanup on UART testbench

--- ve/wb_uart/tb/wb_uart_tb.sv
+++ ve/wb_uart/tb/wb_uart_tb.sv
@@ -7,10 +7,14 @@
  * 
  * TODO: Add module documentation
  */
+`ifdef HAVE_UVM
 `include "uvm_macros.svh"
+`endif
 module wb_uart_tb(input clk);
+`ifdef HAVE_UVM
 	import uvm_pkg::*;
 	import wb_uart_tests_pkg::*;
+`endif
 	
 `ifdef HAVE_HDL_CLKGEN
 		reg clk_r = 0;
@@ -37,18 +41,24 @@ module wb_uart_tb(input clk);
 		end
 	end
 	
+	wb_if #(
+		.WB_ADDR_WIDTH  (WB_ADDR_WIDTH ), 
+		.WB_DATA_WIDTH  (WB_DATA_WIDTH )
+		) u_bfm2uart ();
+	
+	
 	wb_master_bfm #(
 		.WB_ADDR_WIDTH  (WB_ADDR_WIDTH ), 
 		.WB_DATA_WIDTH  (WB_DATA_WIDTH )
 		) u_wb_bfm (
-		.clk            (clk           ), 
-		.rstn           (rstn          ), 
-		.master         (master        ));
+		.clk            (clk              ), 
+		.rstn           (rstn             ), 
+		.master         (u_bfm2uart.master));
 
-	wb_uart wb_uart (
-		.clk        (clk       ), 
-		.rstn       (~rst      ), 
-		.s          (s         ), 
+	wb_uart u_uart (
+		.clk        (clk             ), 
+		.rstn       (~rst            ), 
+		.s          (u_bfm2uart.slave), 
 		.int_o      (int_o     ), 
 		.stx_pad_o  (stx_pad_o ), 
 		.srx_pad_i  (srx_pad_i ), 
@@ -58,10 +68,12 @@ module wb_uart_tb(input clk);
 		.dsr_pad_i  (dsr_pad_i ), 
 		.ri_pad_i   (ri_pad_i  ), 
 		.dcd_pad_i  (dcd_pad_i ));
-	
+
+`ifdef HAVE_UVM
 	initial begin
 		run_test();
 	end
+	`endif
 
 endmodule
 
